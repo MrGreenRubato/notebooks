a3fa370cf473ef95a6250c0543265bda787f63f4
MGR-MB <norbert.mauger@gmail.com>
2016-09-08 18:08:48 +0200

    DONE : ML - Linear Models

--- figures.py
+++ figures.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, Normalizer, RobustScaler
 from sklearn.cross_validation import train_test_split
-
+from sklearn.svm import SVC
 
 def plot_2d_separator(classifier, X, fill=False, ax=None, eps=None):
     if eps is None:
@@ -77,6 +77,25 @@ def plot_relative_scaling():
     axes[2].scatter(X_test_scaled_badly[:, 0], X_test_scaled_badly[:, 1], marker='^',
                     c='r', label="test set", s=60)
     axes[2].set_title("improperly scaled data")
+
+
+
+
+def plot_linear_svc_regularization():
+    X, y = make_blobs(centers=2, random_state=4, n_samples=30)
+    # a carefully hand-designed dataset lol
+    y[7] = 0
+    y[27] = 0
+    
+    fig, axes = plt.subplots(1, 3, figsize=(12, 4))
+    
+    for ax, C in zip(axes, [1e-2, 1, 1e2]):
+        ax.scatter(X[:, 0], X[:, 1], s=150, c=np.array(['red', 'blue'])[y])
+        
+        svm = SVC(kernel='linear', C=C).fit(X, y)
+        plot_2d_separator(svm, X, ax=ax, eps=.5)
+        ax.set_title("C = %f" % C)
+
 
 
 if __name__ == '__main__':
